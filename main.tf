e52ea5a0e6871a1150f1cfad7ea2b856e1fff5b1
Kavian Karimzadeh <kavian@Kavians-Mac-mini.local>
2025-09-15 16:55:26 +0200

    change ssh path

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "hcloud_network_subnet" "hc_private_subnet" {
 
 resource "hcloud_ssh_key" "mykey" {
   name       = "hetzner_key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 ## Creating servers
